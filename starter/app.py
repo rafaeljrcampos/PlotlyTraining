70d0bb022ccacdddd80ca4468bd1bbe87215fbfa
Rafael Junior de Campos <rafaeljuniorcampos.2022@gmail.com>
2024-12-05 10:07:28 -0300

    Add table

--- starter/app.py
+++ starter/app.py
@@ -3,11 +3,13 @@ from dash import dcc, html, Input, Output
 import plotly.graph_objs as go
 import numpy as np
 import random
+import pandas as pd
+import re
+from plotly.subplots import make_subplots
 
 # Inicializa o aplicativo
 app = dash.Dash(__name__)
 app.title = "Plotly Training"
-
 # Layout do aplicativo
 app.layout = html.Div(
     style={"display": "flex", "flex-direction": "row", "height": "100vh"},
@@ -31,7 +33,7 @@ app.layout = html.Div(
                     {"label": "Dispersão", "value": "scatter"},
                     {"label": "Indicador", "value": "indicator"},
                     {"label": "Segundo Indicador", "value": "indicatortwo"},
-
+                    {"label": "Tabela", "value": "table"},
                 ],
                 value="line",
                 className="dash-dropdown",
@@ -39,8 +41,6 @@ app.layout = html.Div(
                     "backgroundColor": "white",  
                 },
             ),
-
-
                 html.Br(),
                 html.Label("Ajuste o Valor Máximo Aleatório:"),
                 dcc.Slider(
@@ -74,8 +74,9 @@ app.layout = html.Div(
 def atualizar_grafico(tipo_grafico, max_valor):
     # Dados aleatórios
     x = np.arange(1, random.randint(1, max_valor))
+    if np.all(x < 2):
+        x = np.arange(1, random.randint(1, max_valor))
     y = np.random.randint(1, max_valor, random.randint(1, max_valor))
-
     # Seleção do tipo de gráfico
     if tipo_grafico == "line":
         fig = go.Figure(data=go.Scatter(x=x, y=y, mode="lines", line=dict(color="#ffc547")))
@@ -87,21 +88,55 @@ def atualizar_grafico(tipo_grafico, max_valor):
         fig = go.Figure(go.Indicator(mode="gauge+number",value= random.uniform(1, max_valor),title={'text': "Indicador"},gauge={'axis': {'range': [0, max_valor]},'bar': {'color': "#ffc547"}, 'threshold' : {'line': {'color': "#FF6347", 'width': 4}, 'thickness': 0.75, 'value':max_valor/2}}))
     elif tipo_grafico =='indicatortwo':
         fig = go.Figure(go.Indicator(mode="number+gauge+delta", gauge={'shape': "bullet", 'axis': {'range': [0, max_valor]}, 'bar': {'color': "#ffc547"}},delta={'reference': 300,'increasing':{'color': "#ffc547"},'decreasing':{'color': "#FF6347"}},value=random.uniform(0, max_valor)))
+    elif tipo_grafico == 'table':
+        df = pd.read_excel('dataset/RelatorioOrders-Handwerk-2511.xlsx', skiprows=3, sheet_name='Planilha1', decimal=',')
+        # Criar a tabela
+        df_filtred = df.loc[df['Tipo Venda'] == 'VENDA', ['Cliente (Nome Fantasia)', 'Data Entrega', 'Tipo Venda','R$ Total']]
+        df_filtred['Data Entrega'] = pd.to_datetime(df_filtred['Data Entrega']).dt.strftime('%m/%Y')
+        df_grouped = df_filtred.groupby('Data Entrega').agg({
+            'Cliente (Nome Fantasia)': 'first',  # Mostra a primeira empresa para cada mês
+            'Tipo Venda': 'first',  # Mostra o primeiro tipo de venda para cada mês
+            'R$ Total': 'sum'  # Soma o R$ Total por mês
+        }).reset_index()
+
+        fig = go.Figure(data=[go.Table(
+            header=dict(
+                values=list(df_grouped.columns),
+                fill_color='lightgrey',
+                align='left',
+                font=dict(size=12)
+            ),
+            cells=dict(
+                values=[df_grouped[col] for col in df_grouped.columns],
+                align='left',
+                font=dict(size=10),
+                height=25,
+            )
+        )])
+
     else:
         fig = go.Figure()
-
-    # Configuração de tema escuro
-    fig.update_layout(
-        paper_bgcolor="#121212",
-        plot_bgcolor="#1e1e1e",
-        font_color="#ffffff",
-        title="Gráfico Dinâmico",
-        xaxis=dict(title="Eixo X"),
-        yaxis=dict(title="Eixo Y"),
-    )
+    if tipo_grafico == "table":
+        fig.update_layout(
+            paper_bgcolor="#121212",
+            plot_bgcolor="#1e1e1e",
+            autosize=True,
+            margin=dict(l=20, r=20, t=20, b=20),
+            xaxis=dict(
+                automargin=True
+            ),
+        )
+    else:
+        # Configuração de tema escuro
+        fig.update_layout(
+            paper_bgcolor="#121212",
+            plot_bgcolor="#1e1e1e",
+            font_color="#ffffff",
+            title="Gráfico Dinâmico",
+            xaxis=dict(title="Eixo X"),
+            yaxis=dict(title="Eixo Y"),
+        )
     return fig
-
-
 # Executa o servidor
 if __name__ == "__main__":
     app.run_server(debug=True)
